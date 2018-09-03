1b4c1713d43755d253408f0cfc86a7ac486a6d14
Cedric David <cedric.david@btopenworld.com>
2018-09-03 01:02:21 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,7 +10,7 @@ def key_for_min_value(name_hash)
   name = ""
   name_hash.each do |item, price|
 
-    if lowest_value == "" || item[price] < lowest_value  
+    if lowest_value == "" || item[price] < lowest_value
       lowest_value = item[price]
       name = item
 
