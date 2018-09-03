fed9eb85045f687dad330877606f0c50ef875376
Cedric David <cedric.david@btopenworld.com>
2018-09-03 01:07:30 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,18 +7,18 @@ def key_for_min_value(name_hash)
     return nil
   end
   lowest_value = ""
-  key = ""
+  lowest_key = ""
   name_hash.each do |item, price|
 
     if lowest_value == "" || item[price] < lowest_value
       lowest_value = item[price]
-      key = item
+      lowest_key = item
 
     end
 
   end
 
-  key
+  lowest_ key
 
 
 end
